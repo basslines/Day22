5ba354ef2f20478baf714b34db8f8bdc37cf77db
basslines <admir.osmanovic@gmail.com>
2021-03-29 23:50:34 +0200

    dodao ex10 projekat

---
+++ ex10/src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+import "./Box";
+import Box from "./Box";
+
+function App() {
+  return (
+    <div className="App">
+      <Box size="20" />
+    </div>
+  );
+}
+
+export default App;
